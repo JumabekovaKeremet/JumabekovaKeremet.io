bc7ce938756132a7dcad6a77590b149b2cfc6cad
ДжумабековаКеремет <jumabekovak15@gmail.com>
2021-02-19 15:25:25 +0600

    created variable and output via the console

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let button = document.querySelector('button');
+let input = document.querySelector('input');
+let input = document.querySelector('input');
+let input = document.querySelector('input');
 button.addEventListener('mousedown', function(){
      console.log(input.value);
 });

03ac522bc3dff1b951b7ed88f53bf0690d62f87c
ДжумабековаКеремет <jumabekovak15@gmail.com>
2021-02-19 15:52:45 +0600

    sccreated scripts file js

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 let button = document.querySelector('button');
 let inputName = document.querySelector('.name');
 let inputMessage = document.querySelector('.message');
-let inputColor = document.querySelector('color');
-button.addEventListener('mousedown', function(){
-     console.log(input.value);
+let inputColor = document.querySelector('.color');
+
+button.addEventListener("mousedown", function(){
+let div  = document.createElement('div');
+let spanName = document.createElement('span');
+let spanMessage = document.createElement('span');
+
+spanName.innerText = inputName.value + " ";
+spanMessage.innerText = inputMessage.value + " ";
+spanName.style.color = inputMessage.value;
+div.append(spanNmae, spanMessage);
+document.body.append(div);
+
+fetch("./https://live-chat-countinue-default-rtdb.firebaseio.com/" ,{
+    method: "POST",
+    body: JSON.stringify({
+        name: inputName.value,
+        massage: inputMessage.value,
+        color: inputColor.vaalue,
+    })
+})
 });
